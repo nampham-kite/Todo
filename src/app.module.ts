60671390bb893747655fc742d573326fbee2b6c7
binhpn <binhpn@icrop.com.vn>
2026-07-13 20:34:45 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'app_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
