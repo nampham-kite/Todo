813384b7217a6178b6a7e776c9f3aa9049566ae3
nampn <yadart321@gmail.com>
2026-07-13 20:19:19 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API docs for the project')
+    .setVersion('1.0')
+    .addBearerAuth() // Nếu dùng JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
+
 bootstrap();
